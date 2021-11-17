06d99082d32ba249cab07b54609e055ee585c54d
Graham Roberts <grahamroberts31@gmail.com>
2021-11-17 15:27:45 -0700

    Adds firebase

---
+++ firebase/index.js
@@ -0,0 +1,8 @@
+// Build the App Instance of Firebase
+import { initializeApp } from 'firebase/app';
+
+import firebaseConfig from "./firebaseConfig";
+
+const firebase = initializeApp(firebaseConfig);
+
+export default firebase;
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
-
+import firebase from "../firebase";
 
 export default function Home() {
+  console.log(firebase);
   return (
     <div>
       <header>
